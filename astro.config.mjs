41d5b9ebb5c1fe3557c5420058f37c17ee1906a0
David <dhermes.mail@web.de>
2026-06-08 16:23:53 +0200

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,9 +67,6 @@ export default defineConfig({
     format: 'file', 
   },
   integrations: [
-    indexnow({
-      key: process.env.INDEXNOW_KEY, 
-    }),
     sitemap(),
     expressiveCode({
       themes: siteConfig.themes.include,
